3ce0d651e24ab9fad470e7820f4d8c9c1b74fd99
sergeLabo <sergeblender@labomedia.org>
2022-07-31 18:36:17 +0200

    avec sketch pour panauto esp32 et arduino

---
+++ Arduino/AS5048A-master/examples/AS5048A_arduino/AS5048A_arduino.ino
@@ -0,0 +1,25 @@
+#include <AS5048A.h>
+
+// 10 = SS true = debug
+AS5048A angleSensor(10, true);
+
+void setup()
+{
+	Serial.begin(19200);
+	angleSensor.begin();
+}
+
+void loop()
+{
+	delay(1000);
+
+	float val = angleSensor.getRotationInDegrees();
+	Serial.print("\nGot rotation of: ");
+	Serial.println(val);
+	Serial.println("State: ");
+	angleSensor.printState();
+	Serial.println("Errors: ");
+	Serial.println(angleSensor.getErrors());
+	Serial.println("Diagnostics: ");
+	Serial.println(angleSensor.getDiagnostic());
+}
